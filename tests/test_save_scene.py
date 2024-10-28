f6cc3f5ad803dbb21ae2015c53191db304907872
YoruCathy <cathyye0809@gmail.com>
2024-10-28 19:05:53 -0400

    Working with dressing env.

--- tests/test_save_scene.py
+++ tests/test_save_scene.py
@@ -2,13 +2,14 @@ import pytest
 from pyrcareworld.envs.base_env import RCareWorld
 import pyrcareworld.attributes as attr
 from pyrcareworld.envs.bathing_env import BathingEnv
+from pyrcareworld.envs.dressing_env import DressingEnv
 
 import os
 import sys
 
 def test_save_scene():
   # Initialize the environment with the specified scene file
-  env = BathingEnv(assets=["Collider_Box", "Rigidbody_Sphere"], graphics=False)
+  env = DressingEnv(assets=["Collider_Box", "Rigidbody_Sphere"], graphics=False)
 
   box1 = env.InstanceObject(name="Collider_Box", attr_type=attr.ColliderAttr)
   box1.SetTransform(position=[-0.5, 0.5, 0], scale=[0.1, 1, 1])
@@ -20,15 +21,19 @@ def test_save_scene():
   box4.SetTransform(position=[0, 0.5, -0.5], scale=[1, 1, 0.1])
   sphere = env.InstanceObject(name="Rigidbody_Sphere", attr_type=attr.RigidbodyAttr)
   sphere.SetTransform(position=[0, 0.5, 0], scale=[0.5, 0.5, 0.5])
-
-  env.SaveScene("helpme.json")
   env.ClearScene()
+  env.step(10)
+  env.SaveScene("helpme.json")
+  env.step(10)
+  env.close()
+  # env.ClearScene()
+  # env.Pend()
   
   # Wait for file to write...
-  for _ in range(300):
-    env.step()
+  # for _ in range(300):
+  #   env.step()
 
   current_dir = os.getcwd()
   print(f"Current working directory: {current_dir}")
-  assert os.path.exists("template/Bathing/BathingPlayer_Data")
-  assert os.path.exists("template/Bathing/BathingPlayer_Data/StreamingAssets/SceneData/helpme.json")
+  assert os.path.exists("template/Dressing/DressingPlayer_Data")
+  assert os.path.exists("template/Dressing/DressingPlayer_Data/StreamingAssets/SceneData/helpme.json")
